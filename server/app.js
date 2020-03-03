b144b29ae488c918967d20bd6d04364761b76729
danny <morganz55057@gmail.com>
2020-03-03 16:06:39 +0100

    started the server on port 5000

--- server/app.js
+++ server/app.js
@@ -11,6 +11,19 @@ const app = express();
 // Defining the PORT
 const PORT = process.env.PORT || 5000;
 
+// Defining the Middlewares
+app.use(cors());
+
+// set the static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// BodyParser Midlleware
+app.use(bodyParser.json());
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get('/', (req, res) => {
     return res.json({
         message : "This is node.js backend system"
