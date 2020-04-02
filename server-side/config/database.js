2a6653bb80b42c3e8975fa1366af97576bf93d50
danny <morganz55057@gmail.com>
2020-04-02 04:05:36 +0200

    adding mongoose pic to wiki

--- server-side/config/database.js
+++ server-side/config/database.js
@@ -5,32 +5,41 @@ const dbURL = `mongodb://${host}/web_app`;
 
 //connect with the database
 mongoose.connect(dbURL,
-    {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true,
-        useFindAndModify : false})
+    {
+        useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true,
+        useFindAndModify: false
+    })
     .then(() => {
-        success({message: `Database connected successfully to ${dbURL}`,
-            badge : true});
+        success({
+            message: `Database connected successfully to ${dbURL}`,
+            badge: true
+        });
     }).catch(err => {
-    error({message: `Mongoose connection error: ${err}`, badge : true});
+    error({message: `Mongoose connection error: ${err}`, badge: true});
 });
 
 // If the connection throws an error
 mongoose.connection.on('error', (err) => {
-    error({message: `Mongoose connection error: ${err}`, badge : true});
+    error({message: `Mongoose connection error: ${err}`, badge: true});
 });
 
 // When the connection is disconnected
 mongoose.connection.on('disconnected', () => {
-    error({message: 'Mongoose  connection disconnected', badge : true});
+    error({message: 'Mongoose  connection disconnected', badge: true});
 });
 
 // If the Node process ends, close the Mongoose connection
 process.on('SIGINT', () => {
     mongoose.connection.close(() => {
-        error({message: 'Mongoose connection disconnected through app termination',
-            badge : true});
+        error({
+            message: 'Mongoose connection disconnected through app termination',
+            badge: true
+        });
         process.exit(0);
     });
 });
+
+
+
 
 
