44377d4dc2aef7ac08fb1e15d87957feddf51387
danny <m.valentin@students.ephec.be>
2020-04-14 20:50:38 +0200

    add level descriptions in results ui

--- client-side/src/app/components/questions/results/results.component.ts
+++ client-side/src/app/components/questions/results/results.component.ts
@@ -11,6 +11,10 @@ import {QuestionsService} from "../../../services/questions.service";
 })
 export class ResultsComponent implements OnInit {
 
+  private level : any;
+  private userLevel : string;
+  private userInfo : any;
+
   constructor(
     private _flashMessagesService: FlashMessagesService,
     private authService: AuthService,
@@ -32,6 +36,21 @@ export class ResultsComponent implements OnInit {
     this.authService.updateScore(userScore)
       .toPromise()
       .then()
+      .catch(err => {
+        console.log(err);
+      });
+    this.getLevel();
+  }
+
+  getLevel(){
+    this.authService.getProfile()
+      .toPromise()
+      .then( (data: any) => {
+        this.userInfo = data;
+        this.userLevel = data.user.level;
+        this.level = this.questions.getLevelDesc(this.userLevel);
+        console.log(this.level["1"]);
+      })
       .catch(err => {
         console.log(err);
       });
