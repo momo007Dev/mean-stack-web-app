2eeb6f60fa4fd41c6ba71501d75c30940f1a4160
danny <automeedwin@gmail.com>
2020-04-24 05:16:30 +0200

    added passport auth2 v2

--- server-side/controllers/users.js
+++ server-side/controllers/users.js
@@ -289,7 +289,7 @@ const update_user_score = (req, res) => {
 };
 
 const get_user_by_id = (req, res) => {
-    
+
     const {userId} = req.params;
     User.findById(userId)
         .select("_id email role username level password")
